2f6c09f37b8c5654f46b2d04ca8cc2b8288c8919
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-10-18 07:10:19 -0700

    Finished Day 33 created tableview with tab bar and navigation controller to receive data from the json. Parsed the data and showed in the table view cells.

---
+++ HackingWithSwiftProject7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  HackingWithSwiftProject7
+//
+//  Created by user208023 on 10/18/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
---
+++ HackingWithSwiftProject7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  HackingWithSwiftProject7
+//
+//  Created by user208023 on 10/18/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
